14cb86a2e0bd7d0931a7c56cd76decd69cb6e54c
burakekinci <burakekinci76@yandex.com>
2023-08-16 23:02:47 +0300

    New Projet Added with Controller-Model-InMemory Data
    New project created according to BTK Akademi Course "Chapter4 working with Models"

---
+++ BTK-WebApi/bookDemo/Controllers/BooksController.cs
@@ -0,0 +1,32 @@
+﻿using bookDemo.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bookDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllBooks()
+        {
+            var books = ApplicationContext.Books;
+            return Ok(books);
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetBook(int id)
+        {
+            var book = ApplicationContext
+                .Books
+                .Where(b => b.Id.Equals(id))
+                .SingleOrDefault();
+
+            if (book is null)
+                return NotFound();
+
+            return Ok(book);
+        }
+
+    }
+}
---
+++ BTK-WebApi/bookDemo/Data/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using bookDemo.Models;
+
+namespace bookDemo.Data
+{
+    public static class ApplicationContext
+    {
+        //In-Memory Data
+        public static List<Book> Books { get; set; }
+        static ApplicationContext()
+        {
+            Books = new List<Book>()
+            {
+                new Book(){ Id=1, Title="Beyaz Zambak Ülkesinde", Price=100},
+                new Book(){ Id=1, Title="Keloğlan Masalları", Price=120},
+                new Book(){ Id=1, Title="Şahmat", Price=80},
+            };
+        }
+    }
+}
