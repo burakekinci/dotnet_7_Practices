999586c37165fdb7ed5ec262a30305a67896385b
burakekinci <burakekinci76@yandex.com>
2023-09-21 00:12:43 +0300

    EF Core Migration class added
    EF Core paketi kurulmuş olup dbContext için repositoryContext sınıfı oluşturuldu, connectionString ayarlaması yapıldı, codeFirst yaklaşım olduğundan migration  sınıfı eklenmiş olup database güncellenmiştir

---
+++ BTK-WebApi/bsStoreApp/bsStoreApp/WebApi/Models/Repositories/RepositoryContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Models.Repositories
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Book> Books { get; set; }
+
+    }
+}
